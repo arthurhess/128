6ee069b14c0b5272f22cde2f96c8524085520bb7
Arthur Hess <arthur@webgoal.com.br>
2014-03-21 15:13:27 -0300

    Make it a much easier and boring game

--- js/game_manager.js
+++ js/game_manager.js
@@ -139,8 +139,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 128 tile
+          if (merged.value === 128) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
